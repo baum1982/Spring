08bcc4d36bd6982dd4b6678642c6c692bed3e0f1
baum1982 <baum1982@gmail.com>
2013-10-15 17:45:48 +0900

    Config

---
+++ tutorial/src/main/java/org/baum/app/edu/config/ConfigMain.java
@@ -0,0 +1,69 @@
+package org.baum.app.edu.config;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ConfigMain {
+
+	public static class ObjectBean implements BeanNameAware{
+		private String name;
+		
+		
+		
+		@Override
+		public String toString() {
+			return "ObjectBean [name=" +name+ "]" ;
+		}
+
+
+
+		@Override
+		public void setBeanName(String name) {
+			this.name = name;
+			
+		}
+	}
+	
+	public static class CollectionBean {
+		Map<String, Integer> dataMap;
+		List<ObjectBean> dataObjList;
+		
+		public Map<String, Integer> getDataMap() {
+			return dataMap;
+		}
+		public void setDataMap(Map<String, Integer> dataMap) {
+			this.dataMap = dataMap;
+		}
+		
+		public List<ObjectBean> getDataObjList() {
+			return dataObjList;
+		}
+		public void setDataObjList(List<ObjectBean> dataObjList) {
+			this.dataObjList = dataObjList;
+		}
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("classpath:org/baum/app/edu/config/applicationContext.xml");
+		CollectionBean bean = 
+				context.getBean(CollectionBean.class);
+		
+		for (ObjectBean item : bean.getDataObjList()) {
+			System.out.println(item.toString());
+		}
+
+		System.out.println("-----------------------------------------");
+		
+		for (Entry<String, Integer> entry : bean.getDataMap().entrySet()) {
+			System.out.println(entry.getKey() + " : " + entry.getValue());
+		}
+	}
+
+}
